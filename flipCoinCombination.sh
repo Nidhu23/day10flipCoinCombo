55529fa2c53590942fa1acf3a0c42a4443dfa3df
nidhu23 <nidhunanik@gmail.com>
2020-06-03 00:18:48 +0530

    Added: compute winning percentage for singlet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,8 +1,31 @@
-#!/bin/bash -x
+#!/bin/bash 
 echo "Welcome to Flip coin problem "
-if [[ $(($RANDOM%2)) -eq 0 ]]
+
+declare -A singlet
+function flipCoin(){
+	if [[ $(($RANDOM%2)) -eq 0 ]]
+	then
+		return 0
+	else
+		return 1
+fi
+}
+read -p "Enter the number of times you want to flip the coin " num
+for((i=0;i<$num;i++))
+do
+if flipCoin
 then
-        echo "Heads"
+	singlet["Heads"]=$((${singlet[Heads]}+1))
 else
-        echo "Tails"
+	singlet["Tails"]=$((${singlet[Tails]}+1))
+fi
+done
+echo ${singlet[Heads]} ${singlet[Tails]}
+if [[ ${singlet[Heads]} > ${singlet[Tails]} ]]
+then
+	n=`echo "scale=2;${singlet[Heads]}/$num*100" | bc`
+	echo -e H is the winner and winning percentage is $n
+else
+	n=`echo "scale=2;${singlet[Tails]}/$num*100" | bc`
+        echo -e T is the winner and winning percentage is $n
 fi
